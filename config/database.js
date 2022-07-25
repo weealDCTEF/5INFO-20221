50e25c64e10b28c0f616bb10278cca48e88d8a74
weealDCTEF <elynetobr@gmail.com>
2022-07-25 10:37:57 -0300

    updated config

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const uri =
-  "mongodb://diego:diego@cluster0-shard-00-00.opcsh.mongodb.net:27017,cluster0-shard-00-01.opcsh.mongodb.net:27017,cluster0-shard-00-02.opcsh.mongodb.net:27017/?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority";
+  "mongodb://ely:123@cluster0-shard-00-00.opcsh.mongodb.net:27017,cluster0-shard-00-01.opcsh.mongodb.net:27017,cluster0-shard-00-02.opcsh.mongodb.net:27017/?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority";
 //const uri = "mongodb://localhost:27017/info";
 
 mongoose.connect(uri);
